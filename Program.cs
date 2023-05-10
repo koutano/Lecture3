df58e735152d696a54783a1e6298f464e8b195ae
koutano <koutano0311@gmail.com>
2023-05-10 16:01:43 +0900

    add Hand

--- Program.cs
+++ Program.cs
@@ -8,8 +8,18 @@ namespace Lecture3_colabo
 {
     internal class Program
     {
+        enum Hand
+        { 
+        ROCK,
+        SCISSORS,
+        PAPER
+        }
+
         static void Main(string[] args)
         {
         }
+
+       
+
     }
 }
